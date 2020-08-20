175820c1b65137230e969878b9ba800be3f36f0b
Ivan Batalin <morganhold@ya.ru>
2020-08-20 12:20:37 +0500

    Fixed res_speech buffer possible double free on channel format capability

--- debian/asterisk-dev/usr/include/asterisk/build.h
+++ debian/asterisk-dev/usr/include/asterisk/build.h
@@ -6,6 +6,6 @@
 #define BUILD_KERNEL "4.4.0-185-generic"
 #define BUILD_MACHINE "x86_64"
 #define BUILD_OS "Linux"
-#define BUILD_DATE "2020-08-03 18:36:45 UTC"
-#define BUILD_USER "root"
+#define BUILD_DATE "2020-08-12 10:27:32 UTC"
+#define BUILD_USER "director"
 
